ad95ba1ee6b2c9add71d93ef497cbcbf9f87d7e7
nmehanma <nmehanma@gmail.com>
2020-04-10 17:11:49 -0400

    displayed student index to be visible to user interface

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,12 @@
+class StudentsController < ApplicationController
+
+  def index
+    @students = Student.all
+
+  end
+
+end
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'courses#index'
   get 'courses/new', to: 'courses#new'
   get 'about', to: 'pages#about'
+  resources :students, except: [:destroy]
 end
+ 
